c7e8c950fd963fe1e4b940dfbe0ef9312ad83015
JulissaDantes <julissadcj@gmail.com>
2022-07-19 09:53:47 -0400

    check proposal creation event

--- src/agent.ts
+++ src/agent.ts
@@ -9,7 +9,7 @@ import {
 } from "forta-agent";
 
 export const QUORUM_UPDATE_EVENT =
-  "event QuorumNumeratorUpdated(uint256 oldQuorumNumerator, uint256 newQuorumNumerator)";
+  "event ProposalCreated(uint256 proposalId, address proposer, address[] targets,  uint256[] values, string[] signatures, bytes[] calldatas, uint256 startBlock, uint256 endBlock, string description)";
 export const GOVERNOR_ADDRESS = "0xdAC17F958D2ee523a2206206994597C13D831ec7";//todo UPDATE
 let findingsCount = 0;
 
@@ -21,23 +21,24 @@ const handleTransaction: HandleTransaction = async (
   // limiting this agent to emit only 5 findings so that the alert feed is not spammed
   if (findingsCount >= 5) return findings;
 
-  // filter the transaction logs for Governor quorum update transfer events
+  // filter the transaction logs to find a proposal created to lower the quorum
   const quorumUpdateEvents = txEvent.filterLog(
     QUORUM_UPDATE_EVENT,
     GOVERNOR_ADDRESS
   );
 
-  quorumUpdateEvents.forEach((transferEvent) => {
-    // extract transfer event arguments
-    const { oldQuorumNumerator, newQuorumNumerator } = transferEvent.args;
+  quorumUpdateEvents.forEach((newQuorumProposalEvent) => {
+    // extract new porposal event arguments
+    const oldQuorumNumerator = newQuorumProposalEvent.args.oldQuorumNumerator.toString()
+    const newQuorumNumerator = newQuorumProposalEvent.args.newQuorumNumerator.toString()
 
     // if quorum is being lowered report it
-    if (oldQuorumNumerator > newQuorumNumerator ) {
+    if (newQuorumProposalEvent.name == "QuorumNumeratorUpdated" && oldQuorumNumerator > newQuorumNumerator) {
       findings.push(
         Finding.fromObject({
-          name: "Quorum lowered",
-          description: `The governor's required quorum has been lowered`,
-          alertId: "FORTA-1",
+          name: "Governor Quorum Numerator Lowered",
+          description: `The governor's required quorum has been lowered ${oldQuorumNumerator} to ${newQuorumNumerator}`,
+          alertId: "FORTA-1",// TODO define this code
           severity: FindingSeverity.Low,
           type: FindingType.Info,
           metadata: {
