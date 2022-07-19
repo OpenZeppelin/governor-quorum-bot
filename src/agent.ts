b3fbe54e76742b788be4b3fc798545044a810e56
JulissaDantes <julissadcj@gmail.com>
2022-07-19 09:21:25 -0400

    check if quorum is lowered

--- src/agent.ts
+++ src/agent.ts
@@ -8,10 +8,9 @@ import {
   FindingType,
 } from "forta-agent";
 
-export const ERC20_TRANSFER_EVENT =
-  "event Transfer(address indexed from, address indexed to, uint256 value)";
-export const TETHER_ADDRESS = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
-export const TETHER_DECIMALS = 6;
+export const QUORUM_UPDATE_EVENT =
+  "event QuorumNumeratorUpdated(uint256 oldQuorumNumerator, uint256 newQuorumNumerator)";
+export const GOVERNOR_ADDRESS = "0xdAC17F958D2ee523a2206206994597C13D831ec7";//todo UPDATE
 let findingsCount = 0;
 
 const handleTransaction: HandleTransaction = async (
@@ -22,30 +21,28 @@ const handleTransaction: HandleTransaction = async (
   // limiting this agent to emit only 5 findings so that the alert feed is not spammed
   if (findingsCount >= 5) return findings;
 
-  // filter the transaction logs for Tether transfer events
-  const tetherTransferEvents = txEvent.filterLog(
-    ERC20_TRANSFER_EVENT,
-    TETHER_ADDRESS
+  // filter the transaction logs for Governor quorum update transfer events
+  const quorumUpdateEvents = txEvent.filterLog(
+    QUORUM_UPDATE_EVENT,
+    GOVERNOR_ADDRESS
   );
 
-  tetherTransferEvents.forEach((transferEvent) => {
+  quorumUpdateEvents.forEach((transferEvent) => {
     // extract transfer event arguments
-    const { to, from, value } = transferEvent.args;
-    // shift decimals of transfer value
-    const normalizedValue = value.div(10 ** TETHER_DECIMALS);
+    const { oldQuorumNumerator, newQuorumNumerator } = transferEvent.args;
 
-    // if more than 10,000 Tether were transferred, report it
-    if (normalizedValue.gt(10000)) {
+    // if quorum is being lowered report it
+    if (oldQuorumNumerator > newQuorumNumerator ) {
       findings.push(
         Finding.fromObject({
-          name: "High Tether Transfer",
-          description: `High amount of USDT transferred: ${normalizedValue}`,
+          name: "Quorum lowered",
+          description: `The governor's required quorum has been lowered`,
           alertId: "FORTA-1",
           severity: FindingSeverity.Low,
           type: FindingType.Info,
           metadata: {
-            to,
-            from,
+              oldQuorumNumerator,
+              newQuorumNumerator,
           },
         })
       );
