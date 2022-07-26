23f859c568686a9531d0745628013a4551939edc
JulissaDantes <julissadcj@gmail.com>
2022-07-26 10:06:02 -0400

    fix file type and imports

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomiclabs/hardhat-ethers");
+require("hardhat-forta");
+const { getJsonRpcUrl } = require("forta-agent");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      forking: {
+        url: getJsonRpcUrl(),
+        blockNumber: 14800611,
+      }
+    }
+  }
+};
