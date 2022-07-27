59da39e013e989151afbf5ad93f6b14d8fb5a425
JulissaDantes <julissadcj@gmail.com>
2022-07-27 12:04:46 -0400

    Phase2.0 (#6)
    * Save created proposals
    
    * update agent
    
    * fix test
    
    * get forked provider and contract
    
    * update tests
    
    * Add check if affects past proposals
    
    * Include affected proposalIds
    
    * include hardhat
    
    * update hardhat config
    
    * Add documentation
    
    * fix file type and imports
    
    * update getAffectedProposals
    
    * update proposalId type
    
    * update tests
    
    * update format
    
    * update README
    
    * check if bug exists
    
    * Update src/agent.ts
    
    Co-authored-by: Francisco <frangio.1@gmail.com>
    
    * Update src/agent.ts
    
    Co-authored-by: Francisco <frangio.1@gmail.com>
    
    * Update src/agent.ts
    
    Co-authored-by: Francisco <frangio.1@gmail.com>
    
    * Update src/agent.ts
    
    Co-authored-by: Francisco <frangio.1@gmail.com>
    
    * update documentation
    
    * Support different vote count
    
    * Update result type
    
    Co-authored-by: Francisco <frangio.1@gmail.com>

--- src/agent.spec.ts
+++ src/agent.spec.ts
@@ -4,11 +4,11 @@ import {
   Finding,
   HandleTransaction,
   createTransactionEvent,
-  ethers,
 } from "forta-agent";
 import agent, { PROPOSAL_CREATED_EVENT } from "./agent";
 
 describe("proposal creation to lower quorum agent", () => {
+  //test must run on block 14800611 temporarily
   let handleTransaction: HandleTransaction;
   const mockTxEvent = createTransactionEvent({} as any);
 
@@ -30,11 +30,12 @@ describe("proposal creation to lower quorum agent", () => {
     });
 
     it("returns a finding if there is a new prosposal to lower quorum", async () => {
-      const oldNumerator = "15";
+      const oldNumerator = "25";
       const newNumerator = "3";
       const newQuorumProposalEvent = {
         name: "ProposalCreated",
         args: {
+          proposer: "0xE8D848debB3A3e12AA815b15900c8E020B863F31",
           oldQuorumNumerator: oldNumerator,
           newQuorumNumerator: newNumerator,
           calldatas: [
@@ -53,7 +54,7 @@ describe("proposal creation to lower quorum agent", () => {
       expect(findings).toStrictEqual([
         Finding.fromObject({
           name: "Governor Quorum Numerator Lowered",
-          description: `The governor's required quorum has been lowered from ${oldNumerator} to ${newNumerator} for ${newQuorumProposalEvent.address}`,
+          description: `The governor's required quorum has been lowered from ${oldNumerator} to ${newNumerator} for ${newQuorumProposalEvent.address}, affected proposalIds: 0xc4715e926c1dd96ed6d703131564450ab353e117129654dd9bd437cf35bb1d18`,
           alertId: "GOVERNOR-QUORUM-UPDATE-PROPOSAL-1",
           severity: FindingSeverity.Low,
           type: FindingType.Info,
@@ -71,7 +72,7 @@ describe("proposal creation to lower quorum agent", () => {
     });
 
     it("returns a finding if there is a new prosposal to lower quorum within different calldatas", async () => {
-      const oldNumerator = "15";
+      const oldNumerator = "25";
       const newNumerator = "3";
       const newQuorumProposalEvent = {
         name: "ProposalCreated",
@@ -82,9 +83,10 @@ describe("proposal creation to lower quorum agent", () => {
             "0x06f3f9e60000000000000000000000000000000000000000000000000000000000000003",
             "0x06f3f9e60000000000000000000000000000000000000000000000000000000000000003",
           ],
+          proposer: "0xE8D848debB3A3e12AA815b15900c8E020B863F31",
           targets: [
             "0x80BAE65E9D56498c7651C34cFB37e2F417C4A703",
-            "0x80BAE65E9D54498c7651C34cFB37e2F417C4A703",
+            "0x80BAE65E9D56498c7651C34cFB37e2F417C4A703",
           ],
         },
         address: "0x80BAE65E9D56498c7651C34cFB37e2F417C4A703",
@@ -98,7 +100,7 @@ describe("proposal creation to lower quorum agent", () => {
       expect(findings).toStrictEqual([
         Finding.fromObject({
           name: "Governor Quorum Numerator Lowered",
-          description: `The governor's required quorum has been lowered from ${oldNumerator} to ${newNumerator} for ${newQuorumProposalEvent.args.targets[0]}`,
+          description: `The governor's required quorum has been lowered from ${oldNumerator} to ${newNumerator} for ${newQuorumProposalEvent.args.targets[0]}, affected proposalIds: 0xc4715e926c1dd96ed6d703131564450ab353e117129654dd9bd437cf35bb1d18`,
           alertId: "GOVERNOR-QUORUM-UPDATE-PROPOSAL-1",
           severity: FindingSeverity.Low,
           type: FindingType.Info,
@@ -110,12 +112,128 @@ describe("proposal creation to lower quorum agent", () => {
         }),
         Finding.fromObject({
           name: "Governor Quorum Numerator Lowered",
-          description: `The governor's required quorum has been lowered from ${oldNumerator} to ${newNumerator} for ${newQuorumProposalEvent.args.targets[1]}`,
+          description: `The governor's required quorum has been lowered from ${oldNumerator} to ${newNumerator} for ${newQuorumProposalEvent.args.targets[1]}, affected proposalIds: 0xc4715e926c1dd96ed6d703131564450ab353e117129654dd9bd437cf35bb1d18`,
           alertId: "GOVERNOR-QUORUM-UPDATE-PROPOSAL-1",
           severity: FindingSeverity.Low,
           type: FindingType.Info,
           metadata: {
             address: newQuorumProposalEvent.args.targets[1],
+            oldQuorumNumerator: newQuorumProposalEvent.args.oldQuorumNumerator,
+            newQuorumNumerator: newQuorumProposalEvent.args.newQuorumNumerator,
+          },
+        }),
+      ]);
+      expect(mockTxEvent.filterLog).toHaveBeenCalledTimes(1);
+      expect(mockTxEvent.filterLog).toHaveBeenCalledWith(
+        PROPOSAL_CREATED_EVENT
+      );
+    });
+
+    it("returns a finding if there is a new prosposal to lower quorum affecting previous proposals", async () => {
+      const newProposalEvent = {
+        name: "ProposalCreated",
+        args: {
+          proposalId: 1,
+        },
+      };
+      mockTxEvent.filterLog = jest.fn().mockReturnValue([newProposalEvent]);
+      const oldNumerator = "25";
+      const newNumerator = "3";
+      const newQuorumProposalEvent = {
+        name: "ProposalCreated",
+        args: {
+          proposalId: 2,
+          proposer: "0xE8D848debB3A3e12AA815b15900c8E020B863F31",
+          oldQuorumNumerator: oldNumerator,
+          newQuorumNumerator: newNumerator,
+          calldatas: [
+            "0x06f3f9e60000000000000000000000000000000000000000000000000000000000000003",
+          ],
+          targets: ["0x80BAE65E9D56498c7651C34cFB37e2F417C4A703"],
+        },
+        address: "0x80BAE65E9D56498c7651C34cFB37e2F417C4A703",
+      };
+      mockTxEvent.filterLog = jest
+        .fn()
+        .mockReturnValue([newQuorumProposalEvent]);
+
+      const findings = await handleTransaction(mockTxEvent);
+
+      expect(findings).toStrictEqual([
+        Finding.fromObject({
+          name: "Governor Quorum Numerator Lowered",
+          description: `The governor's required quorum has been lowered from ${oldNumerator} to ${newNumerator} for ${newQuorumProposalEvent.address}, affected proposalIds: 0xc4715e926c1dd96ed6d703131564450ab353e117129654dd9bd437cf35bb1d18`,
+          alertId: "GOVERNOR-QUORUM-UPDATE-PROPOSAL-1",
+          severity: FindingSeverity.Low,
+          type: FindingType.Info,
+          metadata: {
+            address: newQuorumProposalEvent.address,
+            oldQuorumNumerator: newQuorumProposalEvent.args.oldQuorumNumerator,
+            newQuorumNumerator: newQuorumProposalEvent.args.newQuorumNumerator,
+          },
+        }),
+      ]);
+      expect(mockTxEvent.filterLog).toHaveBeenCalledTimes(1);
+      expect(mockTxEvent.filterLog).toHaveBeenCalledWith(
+        PROPOSAL_CREATED_EVENT
+      );
+    });
+
+    it("returns a finding if there is a new prosposal to lower quorum affecting previous proposals in real contract", async () => {
+      // steps
+      // deploy governance contract and mint tokens to propose and vote
+      const mockGovernorContract = {
+        quorumNumerator: jest.fn(),
+        state: jest.fn(),
+        updateQuorumNumerator: jest.fn(),
+      };
+      // create proposal
+      const newProposalEvent = {
+        name: "ProposalCreated",
+        args: {
+          proposalId: 1,
+        },
+      };
+      mockTxEvent.filterLog = jest.fn().mockReturnValue([newProposalEvent]);
+      // defeat it
+      mockGovernorContract.state.mock.results = [
+        {
+          type: "return",
+          value: 3,
+        },
+        {
+          type: "return",
+          value: 4,
+        },
+      ];
+      // create quorum lowering proposal
+      const newQuorumProposalEvent = {
+        name: "ProposalCreated",
+        args: {
+          proposer: "0xE8D848debB3A3e12AA815b15900c8E020B863F31",
+          oldQuorumNumerator: "25",
+          newQuorumNumerator: "3",
+          calldatas: [
+            "0x06f3f9e60000000000000000000000000000000000000000000000000000000000000003",
+          ],
+          targets: ["0x80BAE65E9D56498c7651C34cFB37e2F417C4A703"],
+        },
+        address: "0x80BAE65E9D56498c7651C34cFB37e2F417C4A703",
+      };
+      mockTxEvent.filterLog = jest
+        .fn()
+        .mockReturnValue([newQuorumProposalEvent]);
+
+      const findings = await handleTransaction(mockTxEvent);
+      expect(findings).toStrictEqual([
+        Finding.fromObject({
+          name: "Governor Quorum Numerator Lowered",
+          description: `The governor's required quorum has been lowered from 25 to 3 for ${newQuorumProposalEvent.address}, affected proposalIds: 0xc4715e926c1dd96ed6d703131564450ab353e117129654dd9bd437cf35bb1d18`,
+          alertId: "GOVERNOR-QUORUM-UPDATE-PROPOSAL-1",
+          severity: FindingSeverity.Low,
+          type: FindingType.Info,
+          metadata: {
+            address: newQuorumProposalEvent.address,
             oldQuorumNumerator: newQuorumProposalEvent.args.oldQuorumNumerator,
             newQuorumNumerator: newQuorumProposalEvent.args.newQuorumNumerator,
           },
--- src/agent.ts
+++ src/agent.ts
@@ -1,20 +1,110 @@
+import type {} from "@nomiclabs/hardhat-ethers";
+import { BigNumber } from "ethers";
 import {
-  BlockEvent,
   Finding,
-  HandleBlock,
   HandleTransaction,
   TransactionEvent,
   FindingSeverity,
   FindingType,
   LogDescription,
-  getJsonRpcUrl,
+  ethers,
 } from "forta-agent";
-import Web3 from "web3";
+import { network } from "hardhat";
 import keccak256 from "keccak256";
 
 export const PROPOSAL_CREATED_EVENT =
   "event ProposalCreated(uint256 proposalId, address proposer, address[] targets,  uint256[] values, string[] signatures, bytes[] calldatas, uint256 startBlock, uint256 endBlock, string description)";
+
+const TOKEN_ABI = [
+  {
+    inputs: [{ internalType: "uint256", name: "blockNumber", type: "uint256" }],
+    name: "getPastTotalSupply",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+];
 const ABI = [
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "proposalId",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "address",
+        name: "proposer",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "address[]",
+        name: "targets",
+        type: "address[]",
+      },
+      {
+        indexed: false,
+        internalType: "uint256[]",
+        name: "values",
+        type: "uint256[]",
+      },
+      {
+        indexed: false,
+        internalType: "string[]",
+        name: "signatures",
+        type: "string[]",
+      },
+      {
+        indexed: false,
+        internalType: "bytes[]",
+        name: "calldatas",
+        type: "bytes[]",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "startBlock",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "endBlock",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "string",
+        name: "description",
+        type: "string",
+      },
+    ],
+    name: "ProposalCreated",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "blockNumber",
+        type: "uint256",
+      },
+    ],
+    name: "quorumNumerator",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
   {
     inputs: [],
     name: "quorumNumerator",
@@ -28,18 +118,156 @@ const ABI = [
     stateMutability: "view",
     type: "function",
   },
+  {
+    inputs: [{ internalType: "uint256", name: "proposalId", type: "uint256" }],
+    name: "state",
+    outputs: [
+      { internalType: "enum IGovernor.ProposalState", name: "", type: "uint8" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint256", name: "newQuorumNumerator", type: "uint256" },
+    ],
+    name: "updateQuorumNumerator",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "proposalId", type: "uint256" }],
+    name: "proposalVotes",
+    outputs: [
+      { internalType: "uint256", name: "againstVotes", type: "uint256" },
+      { internalType: "uint256", name: "forVotes", type: "uint256" },
+      { internalType: "uint256", name: "abstainVotes", type: "uint256" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "proposalId", type: "uint256" }],
+    name: "proposalSnapshot",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "quorumDenominator",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "token",
+    outputs: [{ internalType: "contract IVotes", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "COUNTING_MODE",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    stateMutability: "pure",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "proposalId",
+        type: "uint256",
+      },
+    ],
+    name: "proposals",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        internalType: "address",
+        name: "proposer",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "eta",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "startBlock",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "endBlock",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "forVotes",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "againstVotes",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "abstainVotes",
+        type: "uint256",
+      },
+      {
+        internalType: "bool",
+        name: "canceled",
+        type: "bool",
+      },
+      {
+        internalType: "bool",
+        name: "executed",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
 ];
 const FUNCTION_SELECTOR =
   "0x" + keccak256("updateQuorumNumerator(uint256)").toString("hex", 0, 4);
 
 type QuorumUpdateProposal = {
   target: string;
-  oldQuorumNumerator: number;
-  newQuorumNumerator: number;
+  oldQuorumNumerator: BigNumber;
+  newQuorumNumerator: BigNumber;
 };
 
+// Returns a governor contract using ethers provider pointing to a forked version of the chain
+async function getContract(
+  userAddress: string,
+  contractAddress: string,
+  token: boolean
+): Promise<ethers.Contract> {
+  const provider = await new ethers.providers.Web3Provider(
+    network.provider as any
+  );
+  return await new ethers.Contract(
+    contractAddress,
+    token ? TOKEN_ABI : ABI,
+    provider.getSigner(userAddress)
+  );
+}
+// Return current and proposed numerator value for address
 async function getQuorumUpdateValues(
-  event: LogDescription
+  event: LogDescription,
+  governor: ethers.Contract
 ): Promise<QuorumUpdateProposal[]> {
   const { calldatas, targets } = event.args;
   const response: QuorumUpdateProposal[] = [];
@@ -47,15 +275,9 @@ async function getQuorumUpdateValues(
     for (let i = 0; i < calldatas.length; i++) {
       const calldata = calldatas[i];
       if (calldata.startsWith(FUNCTION_SELECTOR)) {
-        const newQuorumNumerator = Number("0x" + calldata.slice(10));
+        const newQuorumNumerator = BigNumber.from("0x" + calldata.slice(10));
 
-        const web3 = await new Web3(getJsonRpcUrl());
-
-        const governor = await new web3.eth.Contract(ABI as any, event.address);
-
-        let oldQuorumNumerator = await governor.methods
-          .quorumNumerator()
-          .call();
+        let oldQuorumNumerator = await governor["quorumNumerator()"]();
 
         response.push({
           target: targets[i],
@@ -68,37 +290,118 @@ async function getQuorumUpdateValues(
   return response;
 }
 
+// Gets all defeated proposals
+async function getDefeatedProposals(
+  governor: ethers.Contract
+): Promise<BigNumber[]> {
+  const defeatedProposals = [];
+  const eventFilter = await governor.filters.ProposalCreated();
+  const proposalsEvents = await governor.queryFilter(eventFilter);
+
+  // Save all defeated proposals
+  for (const proposalEvent of proposalsEvents) {
+    const { proposalId } = proposalEvent.args as any;
+    const state = await governor.state(proposalId);
+    if (state == 3) {
+      defeatedProposals.push(proposalId);
+    }
+  }
+  return defeatedProposals;
+}
+
+// Returns proposals whose state changed after the quorum update
+async function getAffectedProposals(
+  governor: ethers.Contract,
+  quorumNumerator: number
+): Promise<string[]> {
+  const result: string[] = [];
+  const currentDefeatedProposals: BigNumber[] = await getDefeatedProposals(
+    governor
+  );
+
+  // Simulate the contract.state() check using known new parameter
+  for (const proposalId of currentDefeatedProposals) {
+    const countingMode = await governor.COUNTING_MODE();
+    const isCountingSimple = countingMode.indexOf("abstain") !== -1;
+    const votes = isCountingSimple
+      ? await governor.proposalVotes(proposalId)
+      : await governor.proposals(proposalId);
+    //Check if vote succeeded
+    if (votes.forVotes > votes.againstVotes) {
+      const voteCount = isCountingSimple
+        ? votes.forVotes.add(votes.abstainVotes)
+        : votes.forVotes;
+      const snapshot = await governor.proposalSnapshot(proposalId);
+      //Check quorum
+      const quorumDenominator = await governor.quorumDenominator();
+      const tokenAddress = await governor.token();
+      const token = await getContract(governor.address, tokenAddress, true);
+      const supply = await token.getPastTotalSupply(snapshot); //use snapshot here to get the supply
+      const quorum = supply.mul(quorumNumerator).div(quorumDenominator);
+      if (quorum.lte(voteCount)) {
+        result.push(proposalId.toHexString());
+      }
+    }
+  }
+  return result;
+}
+
 const handleTransaction: HandleTransaction = async (
   txEvent: TransactionEvent
 ) => {
   const findings: Finding[] = [];
 
-  // filter the transaction logs to find a proposal created to lower the quorum
+  // Filter the transaction logs to find if a proposal is created to lower the quorum
+  // and its execution will affect defeated proposals
   const quorumUpdateEvents = txEvent.filterLog(PROPOSAL_CREATED_EVENT);
-
   for (const newQuorumProposalEvent of quorumUpdateEvents) {
     if (newQuorumProposalEvent.name == "ProposalCreated") {
-      let quorumUpdates = await getQuorumUpdateValues(newQuorumProposalEvent);
+      const { proposer } = newQuorumProposalEvent.args;
+      let newVersion: boolean;
+      const governor = await getContract(
+        proposer,
+        newQuorumProposalEvent.address,
+        false
+      );
 
-      for (const update of quorumUpdates) {
-        // if quorum is being lowered report it
-        if (update.oldQuorumNumerator > update.newQuorumNumerator) {
-          const strOldNumerator = update.oldQuorumNumerator.toString();
-          const strNewNumerator = update.newQuorumNumerator.toString();
-          findings.push(
-            Finding.fromObject({
-              name: "Governor Quorum Numerator Lowered",
-              description: `The governor's required quorum has been lowered from ${strOldNumerator} to ${strNewNumerator} for ${update.target}`,
-              alertId: "GOVERNOR-QUORUM-UPDATE-PROPOSAL-1",
-              severity: FindingSeverity.Low,
-              type: FindingType.Info,
-              metadata: {
-                oldQuorumNumerator: strOldNumerator,
-                newQuorumNumerator: strNewNumerator,
-                address: update.target,
-              },
-            })
-          );
+      let quorumUpdates = await getQuorumUpdateValues(
+        newQuorumProposalEvent,
+        governor
+      );
+
+      try {
+        await governor["quorumNumerator(uint256)"](1);
+        newVersion = true;
+      } catch (Exception) {
+        newVersion = false;
+      }
+      if (!newVersion) {
+        for (const update of quorumUpdates) {
+          // if quorum is being lowered report it
+          if (update.oldQuorumNumerator.gt(update.newQuorumNumerator)) {
+            const strOldNumerator = update.oldQuorumNumerator.toString();
+            const strNewNumerator = update.newQuorumNumerator.toString();
+            const affectedProposald = await getAffectedProposals(
+              governor,
+              Number(strNewNumerator)
+            );
+            if (affectedProposald.length > 0) {
+              findings.push(
+                Finding.fromObject({
+                  name: "Governor Quorum Numerator Lowered",
+                  description: `The governor's required quorum has been lowered from ${strOldNumerator} to ${strNewNumerator} for ${update.target}, affected proposalIds: ${affectedProposald}`,
+                  alertId: "GOVERNOR-QUORUM-UPDATE-PROPOSAL-1",
+                  severity: FindingSeverity.Low,
+                  type: FindingType.Info,
+                  metadata: {
+                    oldQuorumNumerator: strOldNumerator,
+                    newQuorumNumerator: strNewNumerator,
+                    address: update.target,
+                  },
+                })
+              );
+            }
+          }
         }
       }
     }
